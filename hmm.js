fa54e39b8ca8785e2338fa0b9980e11eca2bd06e
jekto_vatimeliju <jekto.vatimeliju@gmail.com>
2021-01-06 05:30:54 +0900

    User names and stuffs like that should be in STATE

--- hmm.js
+++ hmm.js
@@ -72,3 +72,62 @@ function init() {
     ctx.restore();
 
 }
+
+function depict_board(board, focus) {
+    let ans = "";
+    for (key in board) {
+        ans += fooo(key, ...board[key], focus == key)
+    }
+
+    document.getElementById("pieces_inner").innerHTML = ans;
+}
+
+const profs = [
+    "船", "無", "兵", "弓", "車", "虎", "馬", "筆", "巫", "将", "王", "皇"
+];
+function fooo(coord, color_and_prof, rotated, is_bold) {
+    const column = {
+        K: 0,
+        L: 1,
+        N: 2,
+        T: 3,
+        Z: 4,
+        X: 5,
+        C: 6,
+        M: 7,
+        P: 8
+    }[coord[0]];
+    const row = {
+        IA: 8,
+        AU: 7,
+        AI: 6, Y: 5, O: 4, U: 3, I: 2, E: 1, A: 0
+    }[coord.slice(1)];
+    const left = left_margin + 43 * (column - 0.5);
+    const top = top_margin + 43 * (row - 0.5);
+    if (color_and_prof === "皇") {
+        const x = profs.indexOf("皇") * -100 - 27;
+        const y = is_bold ? 0 : -277;
+        const color_path = "ゴシック駒";
+        return `
+        <div style="position: absolute; left: ${left}px; top: ${top}px; transform: scale(0.26) ${"rotate(90deg)"}">
+            <div
+                style="width: 87px; height: 87px; background-position-x: ${x}px; background-position-y: ${y}px; background-image: url(${color_path}.svg); ">
+            </div>
+        </div>`;
+    } else {
+        const [color, prof] = color_and_prof;
+        const x = profs.indexOf(prof) * -100 - 27;
+        const y = is_bold ? 0 : -277;
+        const color_path = {
+            "黒": "ゴシック駒",
+            "赤": "ゴシック駒_赤",
+        }[color];
+        return `
+        <div style="position: absolute; left: ${left}px; top: ${top}px; transform: scale(0.26) ${rotated ? "rotate(180deg)" : ""}">
+            <div
+                style="width: 87px; height: 87px; background-position-x: ${x}px; background-position-y: ${y}px; background-image: url(${color_path}.svg); ">
+            </div>
+        </div>`;
+    }
+
+}
