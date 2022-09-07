67033fce0d00a2ec9f5e646c62c0f95149176f61
sozysozbot / hsjoihs <sozysozbot@gmail.com>
2022-09-07 18:28:19 +0900

    Move stuffs to the docs/ folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.ts',
   module: {
     rules: [
@@ -16,7 +17,7 @@ module.exports = {
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
   },
   devtool: 'inline-source-map',
 };
